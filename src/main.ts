aefedd80029eb02cf2ad6d5a9a615a76835e23d9
d.filyukov <d.filyukov@lucky-team.pro>
2023-01-12 22:05:02 +0100

    Module3: Implemented mentor's fixes

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
-import 'reflect-metadata';
-import { Container } from 'inversify';
-import { LoggerInterface } from './common/logger/logger.interface.js';
-import LoggerService from './common/logger/logger.service.js';
-import { Component } from './types/component.types.js';
-import { ConfigInterface } from './common/config/config.interface.js';
-import ConfigService from './common/config/config.service.js';
+import { applicationContainer } from './common/container/container.js';
 import Application from './app/application.js';
-
-const applicationContainer = new Container();
-applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
-applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
-applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+import { Component } from './types/component.types.js';
 
 const application = applicationContainer.get<Application>(Component.Application);
 await application.init();
